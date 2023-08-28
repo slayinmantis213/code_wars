fda4aff69fc5d221ef5a62f6993095f02deba11d
Joshua Bliek <jbliek213@yahoo.com>
2023-08-28 10:27:46 -0700

    algo push

---
+++ algo8-28.js
@@ -0,0 +1,36 @@
+/* 
+  String: Reverse
+
+  Given a string,
+  return a new string that is the given string reversed
+*/
+
+const str1 = "creature";
+const expected1 = "erutaerc";
+
+const str2 = "dog";
+const expected2 = "god";
+
+const str3 = "hello";
+const expected3 = "olleh";
+
+const str4 = "";
+const expected4 = "";
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+function reverseString(str) {
+    newStr = '';
+    for(var i = str.length-1; i >= 0; i--){
+        newStr += str.charAt(i);
+    }
+    return newStr;
+}
+
+console.log(reverseString(str1))
+/*****************************************************************************/
