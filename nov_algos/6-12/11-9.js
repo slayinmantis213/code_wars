1d809b1aace9aceeaf0da531470336f24da81f65
Joshua Bliek <jbliek213@yahoo.com>
2023-11-09 10:31:54 -0800

    11-9algo

---
+++ nov_algos/6-12/11-9.js
@@ -0,0 +1,243 @@
+class DLLNode {
+    /**
+     * Executed when the new keyword is used to construct a new node instance.
+     * @param {any} data The data the new node will store.
+     */
+    constructor(data) {
+      this.data = data;
+      /**
+       * By default a new node instance will not be connected to any other nodes,
+       * these properties will be set after instantiation to connect the node to
+       * a list. However, the head prev should remain null.
+       *
+       * @type {DLLNode|null}
+       */
+      this.prev = null;
+      /** @type {DLLNode|null} */
+      this.next = null;
+    }
+  }
+  
+  /**
+   * A class to represent a doubly linked list and contain all of it's methods.
+   * A doubly linked list is a singly linked list that can be traversed in both
+   * directions.
+   */
+  class DoublyLinkedList {
+    /**
+     * Executed when the new keyword is used to construct a new DoublyLInkedList
+     * instance that inherits these methods and properties.
+     */
+    constructor() {
+      // The list is empty to start.
+      /** @type {DLLNode|null} */
+      this.head = null;
+      /** @type {DLLNode|null} */
+      this.tail = null;
+    }
+  
+    /**
+     * Creates a new node and adds it at the front of this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data for the new node.
+     * @returns {DoublyLinkedList} This list.
+     */
+    insertAtFront(data) {
+      const newHead = new DLLNode(data);
+  
+      if (this.isEmpty()) {
+        this.head = newHead;
+        this.tail = newHead;
+      } else {
+        const oldHead = this.head;
+        oldHead.prev = newHead;
+        newHead.next = oldHead;
+        this.head = newHead;
+      }
+      return this;
+    }
+  
+    insertAtFront2(data) {
+      const newHead = new DLLNode(data);
+  
+      if (this.isEmpty()) {
+        this.head = newHead;
+        this.tail = newHead;
+        return this;
+      }
+  
+      this.head.prev = newHead;
+      newHead.next = this.head;
+      this.head = newHead;
+      return this;
+    }
+  
+    /**
+     * Creates a new node and adds it at the back of this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data for the new node.
+     * @returns {DoublyLinkedList} This list.
+     */
+    insertAtBack(data) {
+      const newTail = new DLLNode(data);
+  
+      if (this.isEmpty()) {
+        // if no head set the newTail to be both the head and the tail
+        this.head = newTail;
+        this.tail = newTail;
+      } else {
+        this.tail.next = newTail;
+        newTail.prev = this.tail;
+  
+        this.tail = newTail;
+      }
+      return this;
+    }
+  
+    // EXTRA
+    /**
+     * Removes the middle node in this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {any} The data of the removed node.
+     */
+    removeMiddleNode() {
+      // when there is only 1 node, it is the middle, remove it.
+      if (!this.isEmpty() && this.head === this.tail) {
+        const removedData = this.head.data;
+        this.head = null;
+        this.tail = null;
+        return removedData;
+      }
+  
+      let forwardRunner = this.head;
+      let backwardsRunner = this.tail;
+  
+      while (forwardRunner && backwardsRunner) {
+        if (forwardRunner === backwardsRunner) {
+          const midNode = forwardRunner;
+          midNode.prev.next = midNode.next;
+          midNode.next.prev = midNode.prev;
+          return midNode.data;
+        }
+  
+        // runners passed each other without stopping on the same node, even length, we can exit early
+        if (forwardRunner.prev === backwardsRunner) {
+          return null;
+        }
+  
+        forwardRunner = forwardRunner.next;
+        backwardsRunner = backwardsRunner.prev;
+      }
+      return null;
+    }
+  
+
+    isEmpty() {
+      return this.head === null;
+    }
+
+    toArray() {
+      const vals = [];
+      let runner = this.head;
+  
+      while (runner) {
+        vals.push(runner.data);
+        runner = runner.next;
+      }
+      return vals;
+    }
+
+
+    insertAtBackMany(items = []) {
+      items.forEach((item) => this.insertAtBack(item));
+      return this;
+    }
+
+
+    insertAfter(targetVal, newVal) {
+        if(this.isEmpty()){
+            return false;
+        }
+        let runner = this.head;
+        let temp;
+        let newNode = new DLLNode(newVal);
+        while(runner){
+            if(runner.data == targetVal){
+                if(runner == this.tail){
+                    runner.next = newNode;
+                    runner.next.prev = runner;
+                    return true;
+                }
+                temp = runner.next;
+                runner.next = newNode;
+                runner.next.prev = runner;
+                runner.next.next = temp;
+                runner.next.next.prev = newNode;
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+  
+    /**
+     * Inserts a new node with the given newVal before the node that has the
+     * given targetVal as it's data.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} targetVal The node data to find.
+     * @param {any} newVal Data for the new node.
+     * @returns {boolean} Indicates if the new node was added.
+     */
+    insertBefore(targetVal, newVal)  {
+        if(this.isEmpty()){
+            return false;
+        }
+        let runner = this.tail;
+        let temp;
+        let newNode = new DLLNode(newVal);
+        while(runner){
+            if(runner.data == targetVal){
+                if(runner == this.head){
+                    temp = runner;
+                    this.head = newNode;
+                    newNode.next = temp;
+                    return true;
+                }
+                temp = runner.prev;
+                runner.prev = newNode;
+                runner.prev.next = runner;
+                runner.prev.prev = temp;
+                runner.prev.prev.next = newNode;
+                return true;
+            }
+            runner = runner.prev;
+        }
+        return false;
+    }
+  
+    // ************************************************** End Work Here
+  }
+  
+  const emptyList = new DoublyLinkedList();
+  
+  /**************** Uncomment these test lists after insertAtBack is created. ****************/
+  // const singleNodeList = new DoublyLinkedList().insertAtBack(1);
+  // const biNodeList = new DoublyLinkedList().insertAtBack(1).insertAtBack(2);
+  // const firstThreeList = new DoublyLinkedList().insertAtBackMany([1, 2, 3]);
+  // const secondThreeList = new DoublyLinkedList().insertAtBackMany([4, 5, 6]);
+  const unorderedList = new DoublyLinkedList().insertAtBackMany([
+    -5,
+    -10,
+    4,
+    -3,
+    6,
+    1,
+    -7,
+    -2,
+  ]);
+  console.log(unorderedList.insertAfter(-2,69));
+  console.log(unorderedList.toArray());
